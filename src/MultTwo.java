54b7790fd21c49129c840d91ad6ca644cc9eb349
tpluu <tpluu@calpoly.edu>
2018-10-05 11:15:34 -0700

    commit1

---
+++ src/MultTwo.java
@@ -0,0 +1,14 @@
+
+public class MultTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(multTwo(3,5));
+	}
+	
+	public static int multTwo(int a, int b) {
+		return a * b;
+	}
+
+}
